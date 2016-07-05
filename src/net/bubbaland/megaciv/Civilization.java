57f1bfaf0a1ae1e17e24c5b57544237c3e280898
Walter Kolczynski <Walter Kolczynski@gmail.com>
2016-07-04 23:34:06 -0400

    Created AST Table and Comparators for Civs.
    Added static AST table to civilization to look up AST ranks.
    
    Added a default compare for Civilization based on AST rank. Added
    additional comparator classes to Civilization to compare by Census or
    AST position.
    
    Started basic structure of Game class.

--- src/net/bubbaland/megaciv/Civilization.java
+++ src/net/bubbaland/megaciv/Civilization.java
@@ -1,13 +1,37 @@
 package net.bubbaland.megaciv;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import net.bubbaland.megaciv.Technology.Type;
 
-public class Civilization {
+public class Civilization implements Comparable<Civilization> {
+
+	private static final HashMap<String, Integer> AST_TABLE;
+	static {
+		AST_TABLE = new HashMap<String, Integer>();
+		AST_TABLE.put("Minoa", 1);
+		AST_TABLE.put("Saba", 2);
+		AST_TABLE.put("Assyria", 3);
+		AST_TABLE.put("Maurya", 4);
+		AST_TABLE.put("Celt", 5);
+		AST_TABLE.put("Babylon", 6);
+		AST_TABLE.put("Carthage", 7);
+		AST_TABLE.put("Dravidia", 8);
+		AST_TABLE.put("Hatti", 9);
+		AST_TABLE.put("Kushan", 10);
+		AST_TABLE.put("Rome", 11);
+		AST_TABLE.put("Persia", 12);
+		AST_TABLE.put("Iberia", 13);
+		AST_TABLE.put("Nubia", 14);
+		AST_TABLE.put("Hellas", 15);
+		AST_TABLE.put("Indus", 16);
+		AST_TABLE.put("Egypt", 17);
+		AST_TABLE.put("Parthia", 18);
+	}
 
 	@JsonProperty("name")
 	private String							name;
@@ -15,26 +39,26 @@ public class Civilization {
 	private String							player;
 	@JsonProperty("census")
 	private int								census;
+	@JsonProperty("astPosition")
+	private int								astPosition;
 	@JsonProperty("techs")
 	private ArrayList<Technology>			techs;
 	@JsonProperty("typeCredits")
 	private final HashMap<Type, Integer>	typeCredits;
 
 	public Civilization(String name, String player) {
-		this.name = name;
-		this.setPlayer(player);
-		this.census = 1;
-		this.techs = new ArrayList<Technology>();
-		this.typeCredits = new HashMap<Type, Integer>();
+		this(name, player, 1, new ArrayList<Technology>(), new HashMap<Type, Integer>(), 0);
 	}
 
 	public Civilization(@JsonProperty("name") String name, @JsonProperty("player") String player,
 			@JsonProperty("census") int census, @JsonProperty("techs") ArrayList<Technology> techs,
-			@JsonProperty("typeCredits") HashMap<Type, Integer> typeCredits) {
+			@JsonProperty("typeCredits") HashMap<Type, Integer> typeCredits,
+			@JsonProperty("astPosition") int astPosition) {
 		this.name = name;
 		this.player = player;
 		this.census = census;
 		this.techs = techs;
+		this.astPosition = astPosition;
 		this.typeCredits = typeCredits;
 	}
 
@@ -48,6 +72,10 @@ public class Civilization {
 
 	public void setCensus(int census) {
 		this.census = census;
+	}
+
+	public void getTech(Technology newTech) {
+		this.techs.add(newTech);
 	}
 
 	public ArrayList<Technology> getTechs() {
@@ -66,4 +94,30 @@ public class Civilization {
 		return this.typeCredits;
 	}
 
+	public int getAST() {
+		return AST_TABLE.get(this.name);
+	}
+
+	@Override
+	/* Compare by AST Rank (default) */
+	public int compareTo(Civilization otherCiv) {
+		return Integer.compare(this.getAST(), otherCiv.getAST());
+	}
+
+}
+
+class CensusComparator implements Comparator<Civilization> {
+	public int compare(Civilization civ1, Civilization civ2) {
+		int result = Integer.compare(civ1.getCensus(), civ2.getCensus());
+		if (result == 0) {
+			result = civ1.compareTo(civ2);
+		}
+		return result;
+	}
+}
+
+class AstPositionComparator implements Comparator<Civilization> {
+	public int compare(Civilization civ1, Civilization civ2) {
+		return Integer.compare(civ1.getAST(), civ2.getAST());
+	}
 }
--- src/net/bubbaland/megaciv/Game.java
+++ src/net/bubbaland/megaciv/Game.java
@@ -1,6 +1,30 @@
 package net.bubbaland.megaciv;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Game {
+
+	private ArrayList<Civilization> civs;
+
+	public Game(HashMap<String, String> startingCivs) {
+		this.civs = new ArrayList<Civilization>();
+		for (String civName : startingCivs.keySet()) {
+			this.addCivilization(civName, startingCivs.get(civName));
+		}
+	}
+
+	private void addCivilization(String civName, String playerName) {
+		this.civs.add(new Civilization(civName, playerName));
+	}
+
+	public ArrayList<Civilization> getCivilizations() {
+		return this.civs;
+	}
+
+	public int nCivilizations() {
+		return this.civs.size();
+	}
 
 
 }
