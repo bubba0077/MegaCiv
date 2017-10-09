c12dd026453540d67542879c9d71e9e8d573d838
Walter Kolczynski <Walter Kolczynski@WinBubba>
2017-10-08 23:46:22 -0400

    New tabs wait for user dialog to close when opening
    Previously, when connecting to a game in-progress without a stored user
    name, the GUI would stop to create new tabs for all the civilizations
    before displaying (or displaying but not allowing entry) into the user
    name dialog. Now the GUI client will wait until a real user name is
    specified before adding the new tabs. This is accomplished using a
    SwingWorker that checks for a user name and then sleeps for 50 ms until
    one is found, at which time a new method is called to add the tabs.
    
    Removed Trade from the starting tabs, since it will be added when the
    data comes in anyway but was messing up which tab would start as visible
    after tabs were added.

--- src/net/bubbaland/megaciv/client/gui/GuiClient.java
+++ src/net/bubbaland/megaciv/client/gui/GuiClient.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 import javax.websocket.ClientEndpoint;
 import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
@@ -50,46 +51,70 @@ public class GuiClient extends GameClient {
 		ArrayList<Civilization.Name> civNamesBefore =
 				( game != null ) ? game.getCivilizationNames() : new ArrayList<Civilization.Name>();
 		super.onMessage(message, session);
+
+
+		GuiClient.this.gui.updateGui(true);
+		ArrayList<Civilization.Name> civNamesAfter = ( GuiClient.this.getGame() != null ) ? GuiClient.this.getGame()
+				.getCivilizationNames() : new ArrayList<Civilization.Name>();
+		boolean civNamesIdentical =
+				civNamesBefore.containsAll(civNamesAfter) && civNamesAfter.containsAll(civNamesBefore);
+
+		/**
+		 * If civilizations have changed, remove and add tabs for civilizations as necessary.
+		 */
+		if (!civNamesIdentical) {
+			( new SwingWorker<Void, Void>() {
+				public Void doInBackground() {
+					while (GuiClient.this.getUser().getUserName() == null || GuiClient.this.getUser().getUserName()
+							.equals(GuiClient.this.getSession().getId().substring(0, 7))) {
+						try {
+							Thread.sleep(50);
+						} catch (final InterruptedException exception) {
+							exception.printStackTrace();
+							System.exit(2);
+						}
+					}
+					return null;
+				}
+
+				public void done() {
+					GuiClient.this.updateTabs();
+				}
+			} ).execute();
+		}
+	}
+
+	private void updateTabs() {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				GuiClient.this.gui.updateGui(true);
-				ArrayList<Civilization.Name> civNamesAfter = ( GuiClient.this.getGame() != null ) ? GuiClient.this
-						.getGame().getCivilizationNames() : new ArrayList<Civilization.Name>();
-				boolean civNamesIdentical =
-						civNamesBefore.containsAll(civNamesAfter) && civNamesAfter.containsAll(civNamesBefore);
 
-				/**
-				 * If civilizations have changed, remove and add tabs for civilizations as necessary.
-				 */
-				if (!civNamesIdentical) {
-					BubbaDragDropTabFrame frame = (BubbaDragDropTabFrame) GuiClient.this.gui.getFirstWindow();
-					frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
-					BubbaDnDTabbedPane pane = frame.getTabbedPane();
+				BubbaDragDropTabFrame frame = (BubbaDragDropTabFrame) GuiClient.this.gui.getFirstWindow();
+				frame.setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
+				BubbaDnDTabbedPane pane = frame.getTabbedPane();
 
-					List<String> allTabs = frame.getTabNames().stream().filter(tabName -> !tabName.startsWith("*"))
-							.collect(Collectors.toList());
+				List<String> allTabs = frame.getTabNames().stream().filter(tabName -> !tabName.startsWith("*"))
+						.collect(Collectors.toList());
 
-					List<String> newTabs = frame.getTabNames().stream()
-							.filter(tabName -> !tabName.startsWith("*") && pane.indexOfTab(tabName) == -1)
-							.collect(Collectors.toList());
+				List<String> newTabs = frame.getTabNames().stream()
+						.filter(tabName -> !tabName.startsWith("*") && pane.indexOfTab(tabName) == -1)
+						.collect(Collectors.toList());
 
-					Arrays.stream(pane.getTabNames()).filter(tabName -> !( allTabs.contains(tabName) ))
-							.forEach(tabName -> pane.remove(pane.indexOfTab(tabName)));
-					;
+				Arrays.stream(pane.getTabNames()).filter(tabName -> !( allTabs.contains(tabName) ))
+						.forEach(tabName -> pane.remove(pane.indexOfTab(tabName)));
+				;
 
-					newTabs.sort(new TabComparator());
+				newTabs.sort(new TabComparator());
 
-					int selectedTab = pane.getSelectedIndex();
+				int selectedTab = pane.getSelectedIndex();
 
-					for (String tabName : newTabs) {
-						// Add the tab to the tabbed pane
-						frame.addTab(tabName);
-					}
-					pane.setSelectedIndex(selectedTab);
-
-					frame.setCursor(Cursor.getDefaultCursor());
+				for (String tabName : newTabs) {
+					// Add the tab to the tabbed pane
+					frame.addTab(tabName);
 				}
+				pane.setSelectedIndex(selectedTab);
+
+				frame.setCursor(Cursor.getDefaultCursor());
 			}
 		});
 	}
