5cbec65663a4413ae22cb8ef2e8cdcf0d8c27b3d
Walter Kolczynski <Walter Kolczynski@WinBubba>
2017-10-08 20:54:16 -0400

    Documentation updates to GameClient
    Updated comments for GameClient to produce appropriate javadoc and
    clarify usage.
    
    Updated GameClient.userNameExists to use streams.

--- src/net/bubbaland/megaciv/client/GameClient.java
+++ src/net/bubbaland/megaciv/client/GameClient.java
@@ -28,49 +28,55 @@ import net.bubbaland.megaciv.game.User;
 import net.bubbaland.megaciv.messages.*;
 import net.bubbaland.sntp.SntpClient;
 
+/**
+ * MegaCivilization game client that handles communication with the game server and updates game data as necessary.
+ * 
+ * @author Walter Kolczynski
+ * 
+ */
+
 @ClientEndpoint(decoders = { ServerMessage.MessageDecoder.class }, encoders = { ClientMessage.MessageEncoder.class })
 public class GameClient implements Runnable {
 
+	// Frequency (in secs) of synchronization of time with server
+	private final static int			SNTP_POLL_INTERVAL			= 30000;
+
+	// Default trade timer length (in secs)
+	private final static int			STARTING_TIMER_LENGTH_SEC	= 300;
+
 	// Format for log timestamps
-	private SimpleDateFormat	timestampFormat;
+	private SimpleDateFormat			timestampFormat;
 
-	// private final String serverUrl;
-	private Session				session;
+	// Connection session with server
+	private Session						session;
 
-	private final SntpClient	sntpClient;
+	// Create an SNTP client to synchronize timing with server
+	private final SntpClient			sntpClient;
+
+	private boolean						isConnected;
+
+	// Game data; updated by server when necessary
+	private volatile Game				game;
+
+	// Users connected to server; updated by server when necessary
+	private volatile ArrayList<User>	userList;
+
+	// User data for this client
+	private User						user;
+
+	// Timer for trade sessions
+	private final Stopwatch				stopwatch;
+
+	// URI for server address
+	private final URI					uri;
 
 	/**
-	 * @return the sntpClient
+	 * Create a new client that connects to the specified server
+	 * 
+	 * @param serverUrl
+	 *            URL of game server
 	 */
-	public SntpClient getSntpClient() {
-		return this.sntpClient;
-	}
-
-	private static int SNTP_POLL_INTERVAL = 30000;
-
-	/**
-	 * @return the session
-	 */
-	public Session getSession() {
-		return this.session;
-	}
-
-	private boolean				isConnected;
-
-	private volatile Game		game;
-
-	private ArrayList<User>		userList;
-
-	private User				user;
-
-	private final static int	STARTING_TIMER_LENGTH_SEC	= 300;
-
-	private final Stopwatch		stopwatch;
-
-	private final URI			uri;
-
 	public GameClient(final String serverUrl) {
-		// this.serverUrl = serverUrl;
 		this.session = null;
 		this.game = null;
 		this.user = new User();
@@ -83,49 +89,152 @@ public class GameClient implements Runnable {
 		this.sntpClient = new SntpClient(this.uri.getHost(), this.uri.getPort() + 1, SNTP_POLL_INTERVAL);
 	}
 
+	/**
+	 * Handle server disconnection
+	 */
+	@OnClose
+	public void connectionClosed() {
+		this.session = null;
+		this.log("Connection closed!");
+		this.isConnected = false;
+	}
+
+	/**
+	 * Get game data. Game data is updated by server when necessary.
+	 * 
+	 * @return The Game data.
+	 */
 	public Game getGame() {
 		return this.game;
 	}
 
+	/**
+	 * Get the currently open session.
+	 * 
+	 * @return The currently open session.
+	 */
+	public Session getSession() {
+		return this.session;
+	}
+
+	/**
+	 * Get the SNTP client (for time synchronization).
+	 * 
+	 * @return The client's SNTP client.
+	 */
+	public SntpClient getSntpClient() {
+		return this.sntpClient;
+	}
+
+	/**
+	 * Get the trading timer.
+	 * 
+	 * @return The client's trade timer.
+	 */
+	public Stopwatch getStopwatch() {
+		return this.stopwatch;
+	}
+
+	/**
+	 * Get this client's user data.
+	 * 
+	 * @return The current user.
+	 */
+	public User getUser() {
+		return this.user;
+	}
+
+	/**
+	 * Get list of users connected to server. This is updated by server as necessary.
+	 * 
+	 * @return A list of the connected users
+	 */
 	public ArrayList<User> getUserList() {
 		return this.userList;
 	}
 
-	public boolean userNameExists(String userName) {
-		for (User user : this.userList) {
-			user.compareTo(this.user);
-			if (user.compareTo(this.user) != 0 && userName.equals(user.getUserName())) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public void setTimestampFormat(SimpleDateFormat timestampFormat) {
-		this.timestampFormat = timestampFormat;
-	}
-
+	/**
+	 * Find out if the client is connected to the server.
+	 * 
+	 * @return A boolean specifying whether client is connected to server.
+	 */
 	public boolean isConnected() {
 		return this.isConnected;
 	}
 
-	@Override
-	public void run() {
-		final ClientManager clientManager = ClientManager.createClient();
+	/**
+	 * Load game save data from file and send to server.
+	 * 
+	 * @param file
+	 *            File containing save data.
+	 */
+	public void loadGame(File file) {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 		try {
-			clientManager.connectToServer(this, this.uri);
-		} catch (DeploymentException | IOException exception) {
-			this.log("Couldn't connect to " + this.uri);
-			// exception.printStackTrace();
-			this.connectionClosed();
+			this.sendMessage(new LoadGameMessage(mapper.readValue(file, Game.class)));
+		} catch (IOException exception) {
+			exception.printStackTrace();
 		}
 	}
 
 	/**
-	 * Initial hook when a client first connects (TriviaServerEndpoint() is automatically called as well)
+	 * Print a message to standard out with a timestamp.
+	 *
+	 * @param message
+	 *            Message to log.
+	 */
+	public void log(String message) {
+		final String timestamp = timestampFormat.format(new Date());
+		// Print message to console
+		System.out.println(timestamp + " " + message);
+	}
+
+	/**
+	 * Handle error in communicating with the server
 	 *
 	 * @param session
+	 *            The session that caused the error.
+	 */
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+		this.log("Error receiving message from " + session.getRequestURI());
+		throwable.printStackTrace();
+	}
+
+	/**
+	 * Handle a new message from the server.
+	 *
+	 * @param message
+	 *            The message from the server.
+	 * @param session
+	 *            The session that received the message.
+	 */
+	@OnMessage
+	public void onMessage(ServerMessage message, Session session) {
+		String messageType = message.getClass().getSimpleName();
+		switch (messageType) {
+			case "GameDataMessage": // Received updated game data
+				this.game = ( (GameDataMessage) message ).getGame();
+				// this.log(this.game.toString());
+				break;
+			case "UserListMessage": // Received an updated user list
+				this.userList = ( (UserListMessage) message ).getUserList();
+				break;
+			case "ServerTimerMessage": // Received a timer synchronization message
+				this.stopwatch.remoteEvent((ServerTimerMessage) message, this.sntpClient.getOffset());
+				break;
+			default:
+		}
+	}
+
+	/**
+	 * Handle when a connection to the server is first established.
+	 *
+	 * @param session
+	 *            The session that has just been activated.
 	 * @param config
+	 *            The configuration used to configure this endpoint.
 	 */
 	@OnOpen
 	public void onOpen(Session session, EndpointConfig config) {
@@ -141,68 +250,42 @@ public class GameClient implements Runnable {
 	}
 
 	/**
-	 * Handle a message from the client
-	 *
-	 * @param message
-	 * @param session
+	 * Run the client by connecting to the server.
+	 * 
 	 */
-	@OnMessage
-	public void onMessage(ServerMessage message, Session session) {
-		String messageType = message.getClass().getSimpleName();
-		switch (messageType) {
-			case "GameDataMessage":
-				this.game = ( (GameDataMessage) message ).getGame();
-				// this.log(this.game.toString());
-				break;
-			case "UserListMessage":
-				this.userList = ( (UserListMessage) message ).getUserList();
-				break;
-			case "ServerTimerMessage":
-				this.stopwatch.remoteEvent((ServerTimerMessage) message, this.sntpClient.getOffset());
-				break;
-			default:
+	@Override
+	public void run() {
+		final ClientManager clientManager = ClientManager.createClient();
+		try {
+			clientManager.connectToServer(this, this.uri);
+		} catch (DeploymentException | IOException exception) {
+			this.log("Couldn't connect to " + this.uri);
+			this.connectionClosed();
 		}
 	}
 
 	/**
-	 * Handle error in communicating with a client
-	 *
-	 * @param session
+	 * Save current game data to a file.
+	 * 
+	 * @param file
+	 *            A file to save game data in.
 	 */
-	@OnError
-	public void onError(Session session, Throwable throwable) {
-		this.log("Error receiving message from " + session.getRequestURI());
-		throwable.printStackTrace();
+	public void saveGame(File file) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(file, this.game);
+		} catch (IOException exception) {
+			exception.printStackTrace();
+		}
 	}
 
 	/**
-	 * Handle a client disconnection
+	 * Send a message to the server. Most messages are updates to the game state based on user input.
 	 *
 	 * @param session
-	 */
-	@OnClose
-	public void connectionClosed() {
-		this.log("Connection closed!");
-		this.isConnected = false;
-	}
-
-	/**
-	 * Display message in the status bar and in console
-	 *
+	 *            The session connected to the server.
 	 * @param message
-	 *            Message to log
-	 */
-	public void log(String message) {
-		final String timestamp = timestampFormat.format(new Date());
-		// Print message to console
-		System.out.println(timestamp + " " + message);
-	}
-
-	/**
-	 * Send a message to the specified client
-	 *
-	 * @param session
-	 * @param message
+	 *            The message to be delivered.
 	 */
 	public void sendMessage(final ClientMessage message) {
 		( new SwingWorker<Void, Void>() {
@@ -219,35 +302,37 @@ public class GameClient implements Runnable {
 		} ).execute();
 	}
 
-	public void saveGame(File file) {
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-			mapper.writeValue(file, this.game);
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		}
+	/**
+	 * Get the date-time format to be used for log timestamps.
+	 * 
+	 * @param timestampFormat
+	 *            The date-time format to be used for log timestamps.
+	 */
+	protected void setTimestampFormat(SimpleDateFormat timestampFormat) {
+		this.timestampFormat = timestampFormat;
 	}
 
-	public void loadGame(File file) {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		try {
-			this.sendMessage(new LoadGameMessage(mapper.readValue(file, Game.class)));
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		}
-	}
-
+	/**
+	 * Set user data for this client.
+	 * 
+	 * @param user
+	 *            The new user data.
+	 */
 	public void setUser(User user) {
 		this.user = user;
 	}
 
-	public User getUser() {
-		return this.user;
+	/**
+	 * Determine whether a user name is already present in the server user list.
+	 * 
+	 * @param userName
+	 *            User name to check.
+	 * @return A boolean specifying whether this is a duplicate user name.
+	 */
+	public boolean userNameExists(String userName) {
+		return this.userList.stream().filter(user -> user.compareTo(this.user) != 0)
+				.anyMatch(user -> userName.equals(user.getUserName()));
 	}
 
-	public Stopwatch getStopwatch() {
-		return this.stopwatch;
-	}
 
 }
